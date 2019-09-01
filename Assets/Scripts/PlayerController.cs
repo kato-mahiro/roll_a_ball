c8efc581cf71eb58727a3cc1718a41f0f0997aed
katomasahiro10 <katomasahiro10@gmail.com>
2019-09-01 15:06:22 +0900

    commit all files

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed; //スピードをエディターから調節できるようにするための変数
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate() // 物理演算のたびに呼ばれる
+    {
+        // プレイヤーのキーボード入力を取得する
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical); 
+
+        rb.AddForce(movement * speed);
+    }
+}
