f9a9b68a2d460987ad3b5d18524b3ce29a6d4cbc
katomasahiro10 <katomasahiro10@gmail.com>
2019-09-08 20:51:06 +0900

    commit all

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed; //スピードをエディターから調節できるようにするための変数
+    public Text countText;
+    public Text winText;
+
     private Rigidbody rb;
+    private int count;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
     }
 
     void FixedUpdate() // 物理演算のたびに呼ばれる
@@ -18,20 +26,30 @@ public class PlayerController : MonoBehaviour
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical); 
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         rb.AddForce(movement * speed);
     }
 
-    
+
     void OnTriggerEnter(Collider other)
     //OnTriggerENter関数はPlayerゲームオブジェクトがTrigger担っているColliderに接触した瞬間に呼ばれる
     {
-        if(other.gameObject.CompareTag("PickUp"))
+        if (other.gameObject.CompareTag("PickUp"))
         {
             other.gameObject.SetActive(false);
-            Debug.Log("衝突した");
+            count++;
+            SetCountText();
+            if(count >= 12)
+            {
+                winText.text = "You Win!";
+            }
         }
     }
-    
+
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+    }
 }
